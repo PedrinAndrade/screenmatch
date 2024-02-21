845ce9511bc173b7951f9f9c709aeb64c584e29a
PedrinAndrade <pedrohandradeg@gmail.com>
2024-02-21 11:26:49 -0300

    Exibindo uma mensagem no navegador

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,2 +1,14 @@
-package br.com.alura.screenmatch.controller;public class SerieController {
+package br.com.alura.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+
+    @GetMapping("/series")//Indica o tipo de requisição
+    public String obterSeries(){
+        return "Aqui vão ser listadas as séries";
+    }
+
 }
