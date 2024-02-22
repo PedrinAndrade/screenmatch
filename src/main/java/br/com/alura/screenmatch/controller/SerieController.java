7959fc25841d0ef1241bc617159cb2d62e01cdb5
PedrinAndrade <pedrohandradeg@gmail.com>
2024-02-22 10:26:34 -0300

    Fornecendo dados para o Front

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,14 +1,27 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
-    @GetMapping("/series")//Indica o tipo de requisição
-    public String obterSeries(){
-        return "Aqui vão ser listadas as séries";
-    }
+    @Autowired
+    private SerieRepository repositorio;
 
+    @GetMapping("/series")//Indica o tipo de requisição
+    public List<SerieDTO> obterSeries(){
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacaoImdb(),
+                        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
 }
